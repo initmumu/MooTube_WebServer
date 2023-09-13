bdd3edd01313337de326b168e50c6e05ab323889
init_mumu <bshookhu@gmail.com>
2023-09-13 15:00:46 +0900

    Project init

---
+++ src/main/java/khu/fssp/mootube/MooTubeApplication.java
@@ -0,0 +1,13 @@
+package khu.fssp.mootube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooTubeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MooTubeApplication.class, args);
+    }
+
+}
